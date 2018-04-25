da2d65f8ba8886331f30d0934cc7abf85994f2e4
askubiy <skubiyap@gmail.com>
2018-04-25 13:52:18 +0300

    Add dates by schedule to csv

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -7,6 +7,10 @@ class Composition < ApplicationRecord
   def generate
     CSV.generate do |csv|
       csv << headers
+      byu_out_info.each do |row|
+        csv << row
+      end
+      csv << []
       date_rows.each do |date_row|
         csv << date_row
       end
@@ -15,18 +19,35 @@ class Composition < ApplicationRecord
 
   def headers
     titles = []
-    titles << "Date"
-    titles << "Another"
+    titles << self.title
     titles.first.prepend("\xEF\xBB\xBF") #Byte Order Mark
     titles
   end
 
+  def byu_out_info
+    rows = []
+    rows << ["ASIN Товара", "B0786ZJ818", "URL"]
+    rows << ["График", schedule, "URL"]
+    rows << ["Сумма", "$758,95", "URL"]
+    rows
+  end
+
   def date_rows
     rows = []
+    rows << ["ПН", "Дата выкупа", "Номер заказа", "Стоимость"]
     date = start_date
+    scheduled = schedule.split('-').map(&:to_i)
+    i = 0
+    order_number = 1
     while date <= end_date
-      rows << [(date).to_s]
+      if scheduled[i]
+        for j in 1..scheduled[i] do
+          rows << [order_number, date.strftime("%d.%m.%Y")]
+          order_number += 1
+        end
+      end
       date = date + 1.day
+      i += 1
     end
     rows
   end
